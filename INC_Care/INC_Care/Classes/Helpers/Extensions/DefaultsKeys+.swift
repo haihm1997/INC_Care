78e54295a146c330e302adde3b75ab178035db52
HoangHai <hoangminhhai1997@gmail.com>
2019-06-12 22:54:08 +0700

    init package & support files

---
+++ INC_Care/INC_Care/Classes/Helpers/Extensions/DefaultsKeys+.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ProjectBase
+//
+//  Created by Hoàng Hải on 10/16/18.
+//  Copyright © 2018 Macbook Pro. All rights reserved.
+//
+
+import SwiftyUserDefaults
+
+extension DefaultsKeys {
+    static let username = DefaultsKey<String>("username")
+    static let userToken = DefaultsKey<String>("userToken")
+}
