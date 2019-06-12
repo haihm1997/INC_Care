ea459f824e0b0ef2cef11e744e615cfc7c6bf375
HoangHai <hoangminhhai1997@gmail.com>
2019-06-12 22:38:56 +0700

    init project

---
+++ INC_Care/INC_Care/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  INC_Care
+//
+//  Created by Hoàng Hải on 6/12/19.
+//  Copyright © 2019 Boogeymen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
